5f2f0e0827d871bc571623f4affe1606d3f3c9ff
adamwilkinson3 <82132359+adamwilkinson3@users.noreply.github.com>
2022-08-06 16:13:32 -0400

    init

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,12 @@ const main = async () => {
         graphiql: true
     }))
 
+    app.use((req, res, next) => {
+        res.append('Access-Control-Allow-Origin', ['*']);
+        res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.append('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    });
     
     app.listen(process.env.PORT || 3001, () => {
         console.log("SERVER RUNNING")
