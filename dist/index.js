6c811bdbb126af2e61d7662465412fa3982d0400
adamwilkinson3 <82132359+adamwilkinson3@users.noreply.github.com>
2022-08-06 16:39:07 -0400

    init

--- dist/index.js
+++ dist/index.js
@@ -38,6 +38,12 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
         schema: Schema_1.schema,
         graphiql: true
     }));
+    app.use((req, res, next) => {
+        res.append('Access-Control-Allow-Origin', ['*']);
+        res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.append('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    });
     app.listen(process.env.PORT || 3001, () => {
         console.log("SERVER RUNNING");
     });
